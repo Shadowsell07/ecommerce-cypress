18adf80b224ba5f627abfaf737c7817c1da91835
Shadowsell07 <Dan.Sellers07@gmail.com>
2025-06-12 15:25:28 +0000

    structure update

--- cypress/e2e/pages/home/home.cy.js
+++ cypress/e2e/pages/home/home.cy.js
@@ -1,4 +1,33 @@
-import HomePage from '../pages/Home';
+import BasePage from '../Base/BasePage';
+import { HOME_PAGE_SELECTORS as SELECTORS } from './constants';
+
+class HomePage extends BasePage {
+    constructor() {
+        super();
+        // Selectors
+        this.searchInput = SELECTORS.SEARCH.INPUT;
+        this.searchButton = SELECTORS.SEARCH.BUTTON;
+        this.cartButton = SELECTORS.CART.BUTTON;
+        this.myAccountDropdown = SELECTORS.ACCOUNT.DROPDOWN;
+    }
+
+    visit() {
+        super.visit('/');
+    }
+
+    search(product) {
+        this.type(this.searchInput, product);
+        this.click(this.searchButton);
+    }
+
+    openCart() {
+        this.click(this.cartButton);
+    }
+
+    openMyAccount() {
+        this.click(this.myAccountDropdown);
+    }
+}
 
 describe('Home Page Tests', () => {
     const homePage = new HomePage();
